fed7fe3b777e0341e9e2f028b53c18b176abddf8
Saurav <sauravrajjha1999@gmail.com>
2021-11-25 15:35:43 +0530

    New SubFile Added

---
+++ SUB.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define rf(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define pi pair<int ,int>
+#define mp make_pair
+#define F first
+#define S second
+#define vvi vector<vector<int>>
+
+int main()
+{
+    int n=12, m=25;
+    cout<<n-m;
+    return 0;
+}
